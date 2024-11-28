ab5f6c15a66e0bd3739c7eaa429952433da7c525
tokiya <e2544733@outlook.com>
2024-11-28 11:31:47 +0800

    feat:add page introduction

---
+++ src/app/introduction/page.tsx
@@ -0,0 +1,53 @@
+import AnimatedComponent from "../components/AnimatedComponent";
+
+
+
+export default function IntroductionPage() {
+    return (
+        <>
+            <AnimatedComponent>
+                <div className="fixed top-0 left-0 w-full h-full overflow-hidden z-[-1]">
+                    <div className="w-[10px] h-[200px]">
+                    </div>
+                    <div className="flex justify-center max-sm:hidden max-md:hidden max-lg:hidden max-xl:hidden">
+                        <div className="w-[1000px] h-[600px] max-xl:h-[400px] max-xl:w-[300px]  bg-[url('/img/chara-left.png')] bg-no-repeat bg-contain bg-left"></div>
+                        <div className="w-[1200px]"></div>
+                        <div className="w-[1000px] h-[600px] max-xl:h-[400px] max-xl:w-[300px] bg-[url('/img/chara-right.png')] bg-no-repeat bg-contain bg-right"></div>
+                    </div>
+                </div>
+                <div className="relative z-[50] w-full h-[1200px] max-sm:h-[1000px] ">
+                    <div className="flex justify-center p-10 items-center text-center w-[1000px] mx-auto text-[50px] font-bold max-sm:w-full max-sm:p-5 max-sm:text-[32px] max-md:w-full max-lg:w-full max-xl:w-full">
+                        Welcome to maimai 大中华区玩家站
+                    </div>
+                    <div className="flex justify-center w-full h-screen ">
+                        <div className="w-[1000px] h-[600px] max-sm:w-[300px] max-sm:h-[600px] max-md:w-[500px] max-2xl:w-[800px] bg-white/50 backdrop-blur-lg shadow-lg rounded-lg p-6 mt-10 transition-all duration-300 ease-in-out hover:bg-white/70 hover:shadow-2xl hover:backdrop-blur-xl hover:scale-105 overflow-auto scrollbar-hide">
+                            <h1 className="text-2xl font-bold text-gray-700">Maimai大中华区玩家站</h1>
+                            <p className="mt-4 text-gray-500 indent-4 mb-3">
+                                我们旨在打造一个为中国Maimai玩家服务的乌蒙大象玩家站，所有开发者都来自wmc群体。
+                            </p>
+                            <h1 className="text-2xl font-bold text-gray-700">它具有什么功能？</h1>
+                            <p className="mt-4 text-gray-500 indent-4">
+                                我们的玩家站将集成大部分的开源maimai项目相关功能，我们诚邀各位maimai第三方项目开发者加入我们的开发团队，共同打造一个更好的玩家站。
+                            </p>
+                            <ul className="list-decimal mt-4 pl-5 text-gray-500 mb-3">
+                                <li>将会展示大量的有关maimai的游戏信息，包括但不限于：机厅信息、更新内容、未来可能的更新计划、机厅活动等等</li>
+                                <li>将会尽可能提供maimai相关工具，包括但不限于：b50查询，随机段位，牌子完成度等等。<br></br>
+                                    <b>注意：我们的这些功能将会在官方提供之后立刻删除</b>
+                                </li>
+                            </ul>
+                            <h1 className="text-2xl font-bold text-gray-700">帮助我们</h1>
+                            <p className="mt-4 text-gray-500 indent-4">
+                                我们设计了一套基础问卷，如果您对问卷有任何希望添加的内容请联系我们，问卷计划将在<b>12.1号</b>发布。
+                                在发布问卷期间，我们依旧会对问卷的内容有所更新。
+                                <a href="mailto:tokiy@tokya.online?subject=问卷反馈" className="text-blue-500 underline hover:text-blue-700">
+                                    向我们发送邮件告知您的意见
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </AnimatedComponent>
+        </>
+    )
+}
--- src/app/questionnaire/page.tsx
+++ src/app/questionnaire/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useEffect, useState } from "react"
+import AnimatedComponent from "../components/AnimatedComponent";
 
 export default function QuestionnairePage() {
   const [dataDict, setDataDict] = useState<{ [key: string]: string }>({});
@@ -45,79 +46,81 @@ export default function QuestionnairePage() {
 
   return (
     <>
-      <img src="/img/bg_shines.png" className="fixed" alt="" />
-      <div className="relative w-full">
-        <div className="fixed top-0 left-0 w-full h-full overflow-hidden z-[-1]">
-          <div className="flex justify-center mt-10">
-            <div className="w-[1000px] h-[600px] max-sm:h-[200px] max-sm:w-[180px] bg-[url('/img/front_left.png')] bg-no-repeat bg-contain bg-left"></div>
-            <div className="w-[1000px] h-[600px] max-sm:h-[100px] max-sm:w-[60px] bg-no-repeat bg-contain bg-left"></div>
-            <div className="w-[1000px] h-[600px] max-sm:h-[200px] max-sm:w-[180px] bg-[url('/img/front_right.png')] bg-no-repeat bg-contain bg-left"></div>
+      <AnimatedComponent>
+        <img src="/img/bg_shines.png" className="fixed" alt="" />
+        <div className="relative w-full">
+          <div className="fixed top-0 left-0 w-full h-full overflow-hidden z-[-1]">
+            <div className="flex justify-center mt-10">
+              <div className="w-[1000px] h-[400px]  max-sm:h-[200px] max-sm:w-[180px] max-md:w-[200px] max-md:h-[200px] max-lg:w-[260px] max-lg:h-[220px] max-xl:w-[400px] max-xl:h-[300px] max-2xl:h-[300px]  bg-[url('/img/front_left.png')] bg-no-repeat bg-contain bg-left"></div>
+              <div className="w-[1000px] h-[400px]  max-sm:h-[100px] max-sm:w-[60px]  max-md:w-[200px] max-md:h-[200px] max-lg:w-[260px] max-lg:h-[220px] max-xl:w-[400px] max-xl:h-[300px] max-2xl:h-[300px]  bg-no-repeat bg-contain bg-left"></div>
+              <div className="w-[1000px] h-[400px]  max-sm:h-[200px] max-sm:w-[180px] max-md:w-[200px] max-md:h-[200px] max-lg:w-[260px] max-lg:h-[220px] max-xl:w-[400px] max-xl:h-[300px] max-2xl:h-[300px]  bg-[url('/img/front_right.png')] bg-no-repeat bg-contain bg-left"></div>
+            </div>
+            <div className="flex justify-center max-sm:hidden max-md:hidden max-lg:hidden">
+              <div className="w-[1000px] h-[600px] max-xl:h-[400px] max-xl:w-[300px]  bg-[url('/img/chara-left.png')] bg-no-repeat bg-contain bg-left"></div>
+              <div className="w-[1200px]"></div>
+              <div className="w-[1000px] h-[600px] max-xl:h-[400px] max-xl:w-[300px] bg-[url('/img/chara-right.png')] bg-no-repeat bg-contain bg-right"></div>
+            </div>
           </div>
-          <div className="flex justify-center max-sm:hidden">
-            <div className="w-[1000px] h-[600px] bg-[url('/img/chara-left.png')] bg-no-repeat bg-contain bg-left"></div>
-            <div className="w-[1200px]"></div>
-            <div className="w-[1000px] h-[600px] bg-[url('/img/chara-right.png')] bg-no-repeat bg-contain bg-right"></div>
+          <div className="w-[400px] h-[200px] max-sm:w-[180px] bg-[url('/img/moon.png')] bg-no-repeat bg-contain bg-center z-10 mx-auto mt-20 max-sm:mt-10 max-sm:pt-10">
+            <div>
+              <img src="/img/logo.png" alt="" />
+            </div>
+          </div>
+          <div className="w-[750px] h-[1200px] max-sm:w-[375px] max-sm:h-[700px] mx-auto bg-[url('/img/main_bg.png')] bg-no-repeat bg-contain rounded-2xl mt-10 max-sm:mt-0 mb-10 p-20 max-sm:p-10 z-10 text-blue-600 font-bold tracking-wider ">
+            <form className="max-sm:h-[500px] overflow-auto scrollbar-hide">
+              <label>1、我们如何称呼您</label><br></br>
+              <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="text" name="name" onChange={handleChange} /><br></br>
+              <label>2、您的邮箱</label><br></br>
+              <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="email" name="email" onChange={handleChange} /><br></br>
+              <label>3、您认为我们需要添加什么样的功能</label><br></br>
+              <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="text" name="addFunction" onChange={handleChange} /><br></br>
+              <label>4、您是否愿意参加到我们的开发中?</label><br></br>
+              <label >(如果您选择了除不愿意之外的任何选项，您将在两天之内收到来自SaltHouse的邀请)</label><br></br>
+              <input className="text-black my-2" type="radio" name="identity" value="QA" onChange={handleChange} />QA(测试)
+              <input className="text-black" type="radio" name="identity" value="MainDevelop" onChange={handleChange} />主要功能开发
+              <input className="text-black" type="radio" name="identity" value="derice" onChange={handleChange} />衍生功能开发<br></br>
+              <input className="text-black my-2" type="radio" name="identity" value="Art" onChange={handleChange} />美工支持
+              <input className="text-black mb-5" type="radio" name="identity" value="NO" onChange={handleChange} />不愿意<br></br>
+              <label >5、如果您希望加入我们的吹水群，请留下QQ号</label><br></br>
+              <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="text" name="qq" onChange={handleChange} /><br></br>
+              <label >6、关于我们乌蒙大象中国站，有什么想说的</label><br></br>
+              <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="text" name="else" onChange={handleChange} /><br></br>
+              <div className="flex justify-center">
+                <button
+                  type="button" // 修改为 type="button" 以避免表单提交
+                  onClick={submitButton} // 点击按钮打开弹窗
+                  className="w-36 h-10 bg-[url('/img/bg_button.png')] bg-no-repeat bg-cover rounded-2xl flex items-center justify-center text-white font-bold text-lg shadow-lg hover:shadow-2xl hover:scale-105 active:scale-95 active:shadow-md transition duration-300 ease-in-out"
+                >
+                  打开弹窗
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        <div className="w-[400px] h-[200px] max-sm:w-[180px] bg-[url('/img/moon.png')] bg-no-repeat bg-contain bg-center z-10 mx-auto mt-20 max-sm:mt-10 max-sm:pt-10">
-          <div>
-            <img src="/img/logo.png" alt="" />
-          </div>
-        </div>
-        <div className="w-[750px] h-[1200px] max-sm:w-[375px] max-sm:h-[700px] mx-auto bg-[url('/img/main_bg.png')] bg-no-repeat bg-contain rounded-2xl mt-10 max-sm:mt-0 mb-10 p-20 max-sm:p-10 z-10 text-blue-600 font-bold tracking-wider ">
-          <form className="max-sm:h-[500px] overflow-auto">
-            <label>1、我们如何称呼您</label><br></br>
-            <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="text" name="name" onChange={handleChange} /><br></br>
-            <label>2、您的邮箱</label><br></br>
-            <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="email" name="email" onChange={handleChange} /><br></br>
-            <label>3、您认为我们需要添加什么样的功能</label><br></br>
-            <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="text" name="addFunction" onChange={handleChange} /><br></br>
-            <label>4、您是否愿意参加到我们的开发中?</label><br></br>
-            <label >(如果您选择了除不愿意之外的任何选项，您将在两天之内收到来自SaltHouse的邀请)</label><br></br>
-            <input className="text-black my-2" type="radio" name="identity" value="QA" onChange={handleChange} />QA(测试)
-            <input className="text-black" type="radio" name="identity" value="MainDevelop" onChange={handleChange} />主要功能开发
-            <input className="text-black" type="radio" name="identity" value="derice" onChange={handleChange} />衍生功能开发<br></br>
-            <input className="text-black my-2" type="radio" name="identity" value="Art" onChange={handleChange} />美工支持
-            <input className="text-black mb-5" type="radio" name="identity" value="NO" onChange={handleChange} />不愿意<br></br>
-            <label >5、如果您希望加入我们的吹水群，请留下QQ号</label><br></br>
-            <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="text" name="qq" onChange={handleChange} /><br></br>
-            <label >6、关于我们乌蒙大象中国站，有什么想说的</label><br></br>
-            <input className="w-full px-4 py-2 my-2 text-gray-700 bg-gradient-to-r from-gray-100 via-white to-gray-100 rounded-lg shadow-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none focus:border-blue-400 hover:shadow-lg transition duration-300" type="text" name="else" onChange={handleChange} /><br></br>
-            <div className="flex justify-center">
+
+        {/* 弹窗 */}
+        {isModalOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white w-[90%] md:w-[400px] rounded-lg shadow-lg p-6 relative">
               <button
-                type="button" // 修改为 type="button" 以避免表单提交
-                onClick={submitButton} // 点击按钮打开弹窗
-                className="w-36 h-10 bg-[url('/img/bg_button.png')] bg-no-repeat bg-cover rounded-2xl flex items-center justify-center text-white font-bold text-lg shadow-lg hover:shadow-2xl hover:scale-105 active:scale-95 active:shadow-md transition duration-300 ease-in-out"
+                onClick={closeModal} // 点击关闭按钮
+                className="absolute top-2 right-2 text-gray-400 hover:text-gray-600 transition duration-200"
               >
-                打开弹窗
+                ✕
+              </button>
+              <h2 className="text-xl font-bold mb-4 text-gray-800">弹窗内容</h2>
+              <img src="/img/chara.png" alt="" />
+              <p className="text-gray-600 mt-10 mb-4 text-center">感谢您的提交</p>
+              <button
+                onClick={closeModal} // 点击关闭按钮
+                className="w-full py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300"
+              >
+                确定
               </button>
             </div>
-          </form>
-        </div>
-      </div>
-
-      {/* 弹窗 */}
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white w-[90%] md:w-[400px] rounded-lg shadow-lg p-6 relative">
-            <button
-              onClick={closeModal} // 点击关闭按钮
-              className="absolute top-2 right-2 text-gray-400 hover:text-gray-600 transition duration-200"
-            >
-              ✕
-            </button>
-            <h2 className="text-xl font-bold mb-4 text-gray-800">弹窗内容</h2>
-            <img src="/img/chara.png" alt="" />
-            <p className="text-gray-600 mt-10 mb-4 text-center">感谢您的提交</p>
-            <button
-              onClick={closeModal} // 点击关闭按钮
-              className="w-full py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300"
-            >
-              确定
-            </button>
           </div>
-        </div>
-      )}
+        )}
+      </AnimatedComponent>ƒ
     </>
   )
 }
