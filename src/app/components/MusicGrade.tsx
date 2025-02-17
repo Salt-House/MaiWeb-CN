3693804e0b468883a57cbc63c412bb14ceaefb26
tokiya <e2544733@outlook.com>
2025-02-17 10:27:23 +0800

    feat:add grade webp and modify MusicGrade.tsx and best/page.tsx

--- src/app/components/MusicGrade.tsx
+++ src/app/components/MusicGrade.tsx
@@ -21,7 +21,10 @@ export default function MusicGrade(props: MusicGradeProps) {
 
   let levelColor: string = 'bg-green-500';
   let nameColor: string = 'bg-green-500';
-  let textstroke = {
+  let fc = null;
+  let fs = null;
+  let achievements = null;
+  let textstroke: React.CSSProperties = {
     textShadow: '-1px -1px 2px rgba(128, 90, 213, 1), 1px -1px 2px rgba(128, 90, 213, 1), -1px 1px 1px rgba(128, 90, 213, 1), 1px 1px 1px rgba(128, 90, 213, 1)'
   };
   let GradeColor = {
@@ -62,27 +65,105 @@ export default function MusicGrade(props: MusicGradeProps) {
       };
       break;
     case 4:
-      levelColor = 'bg-white-500';
-      nameColor = 'text-white';
+      levelColor = 'bg-purple-500';
+      nameColor = 'text-purple-500';
       textstroke = {
-        textShadow: '-2px -2px 4px rgba(255, 255, 255, 1), 2px -2px 4px rgba(255, 255, 255, 1), -2px 2px 2px rgba(255, 255, 255, 1), 2px 2px 2px rgba(255, 255, 255, 1)'
+        textShadow: '-2px -2px 2px rgba(255, 255, 255, 1), 2px -2px 2px rgba(255, 255, 255, 1), -2px 2px 2px rgba(255, 255, 255, 1), 2px 2px 2px rgba(255, 255, 255, 1)'
+        // WebkitTextStroke: '0.8px rgba(255, 255, 255, 1)',  // 白色描边
       };
+      break;
+  }
+  switch (props.fc) {
+    case 0:
+      fc = null;
+      break;
+    case 1:
+      fc = null;
+      break;
+    case 2:
+      fc = '/img/grade/fcp.webp';
+      break;
+    case 3:
+      fc = '/img/grade/fc.webp';
+      break;
+    default:
+      fc = null;
+      break;
+  }
+  switch (props.fs) {
+    case 0:
+      fs = null;
+      break;
+    case 1:
+      fs = null;
+      break;
+    case 2:
+      fs = '/img/grade/fsp.webp';
+      break;
+    case 3:
+      fs = null;
+      break;
+    default:
+      fs = null;
+      break;
+  }
+  switch (true) {
+    case props.achievements >= 100.5:
+      achievements = '/img/grade/sssp.webp';
+      break;
+    case props.achievements >= 100:
+      achievements = '/img/grade/sss.webp';
+      break;
+    case props.achievements >= 99.5:
+      achievements = '/img/grade/ssp.webp';
+      break;
+    case props.achievements >= 99:
+      achievements = '/img/grade/ss.webp';
+      break;
+    case props.achievements >= 98.5:
+      achievements = '/img/grade/sp.webp';
+      break;
+    case props.achievements >= 98:
+      achievements = '/img/grade/s.webp';
+      break;
+    case props.achievements >= 97:
+      achievements = '/img/grade/a.webp';
+      break;
+    case props.achievements >= 95:
+      achievements = '/img/grade/aaa.webp';
+      break;
+    default:
+      achievements = null;
       break;
   }
 
   return (
     <>
-      <Link href={`/music/${props.id}`} className={`w-[160px] h-48 break-words p-2 m-2 border border-gray-300 rounded-lg shadow-md hover:scale-105 hover:shadow-xl duration-300 ease-in-out backdrop-filter backdrop-blur-lg ${levelColor} bg-opacity-30`}>
+      <Link href={`/music/${props.id}`} className={`w-[160px] h-52 break-words p-2 m-2 border border-gray-300 rounded-lg shadow-md hover:scale-105 hover:shadow-xl duration-300 ease-in-out backdrop-filter backdrop-blur-lg ${levelColor} bg-opacity-30`}>
         <h1
           className={`w-full text-xl pl-1 font-bold truncate ${nameColor}`}
           style={textstroke}
         >
           {props.song_name}
         </h1>
-        <h2 className={`text-md pl-1 font-bold ${nameColor}`} style={textstroke}>{props.level}</h2>
-        <h3 className={`text-xl pl-1 tracking-[0.2em] text-gray-600 font-bold ${nameColor}`} style={GradeColor}>{props.achievements}</h3>
-        <img className="size-20 mt-2" src={`${baseUrl}/jacket/${props.id}.png`} alt={props.song_name} />
-        
+        <div className='flex flex-row space-x-2'>
+          <h2 className={`text-md pl-1 font-bold ${nameColor}`} style={textstroke}>{props.level}</h2>
+          <img src={`${achievements}`} className='w-24' alt="" />
+        </div>
+        <h3 className={`text-xl pl-1 tracking-[0.2em] font-bold text-white`} style={GradeColor}>{props.achievements}</h3>
+        <div className='flex h-24 flex-row justify-center items-center '>
+          <div className='border-4 border-white'>
+            <img className="size-20" src={`${baseUrl}/jacket/${props.id}.png`} alt={props.song_name} />
+          </div>
+          <div className='relative h-full flex flex-col justify-center items-center space-y-1'>
+            {fc != null ?
+              <div className='size-10 bg-no-repeat bg-center bg-[length:50px_50px]' style={{ backgroundImage: `url(${fc})` }}></div>
+              : <div className='m-2 size-10 rounded-full bg-gray-500'></div>}
+            {fs != null ?
+              <div className='size-10 bg-no-repeat bg-center bg-[length:50px_50px]' style={{ backgroundImage: `url(${fs})` }}></div>
+              : <div className='m-2 size-10 rounded-full bg-gray-500'></div>}
+          </div>
+        </div>
       </Link >
     </>
 
