590f2785e742bd76b3bc3360e9bc1c207fdb45e7
Raspberry… <118886923+KafuChino123@users.noreply.github.com>
2025-03-04 10:30:48 +0800

    fix: fix display problems

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,9 +71,9 @@ export default function RootLayout({
         <meta name="referrer" content="no-referrer" />
       </Head>
       <body className="font-douyin">
-        <div className="relative h-screen overflow-y-auto pb-safe">
-          <Link href={'/tool/donate'} className="fixed z-[5] bottom-safe right-5 flex flex-col justify-center items-center hover:cursor-pointer transition-all duration-300 hover:bottom-8"><img src="/img/youxibi.png" className="hover:scale-105 transition-all duration-300 ease-in-out" alt="" /><h1 className="text-white font-bold" style={textDonate}>请开发者打一把mai</h1></Link>
-          <Link href={'/tool/feedback'} className="fixed z-[5] bottom-safe left-5 flex flex-col justify-center items-center hover:cursor-pointer transition-all duration-300 hover:bottom-8"><img src="/img/yijianfankui.png" className="hover:scale-105 transition-all duration-300 ease-in-out" alt="" /><h1 className="text-white font-bold" style={textDonate}>意见反馈</h1></Link>
+        <div className="relative h-screen overflow-scroll">
+          <Link href={'/tool/donate'} className="fixed z-[5] bottom-5 right-5 flex flex-col justify-center items-center hover:cursor-pointer transition-all duration-300 hover:bottom-8"><img src="/img/youxibi.png" className="hover:scale-105 transition-all duration-300 ease-in-out" alt="" /><h1 className="text-white font-bold" style={textDonate}>请开发者打一把mai</h1></Link>
+          <Link href={'/tool/feedback'} className="fixed z-[5] bottom-5 left-5 flex flex-col justify-center items-center hover:cursor-pointer transition-all duration-300 hover:bottom-8"><img src="/img/yijianfankui.png" className="hover:scale-105 transition-all duration-300 ease-in-out" alt="" /><h1 className="text-white font-bold" style={textDonate}>意见反馈</h1></Link>
           <AnimatedComponent isVisible={true}>
             {/* BackGround Layer */}
             <div className="fixed top-0 left-0 w-full h-full  z-[-1] bg-[url('/img/bg_shines.png')]">
