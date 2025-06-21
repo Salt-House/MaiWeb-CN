a39898880488b8e81d7f851fbc39c94fd2da39ca
tokiya <e2544733@outlook.com>
2025-06-21 21:08:50 +0800

    feat: add alert in register

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -45,12 +45,12 @@ export default function UserPage() {
         if (statusCode == 201) {
           alert("Register Success")
           window.location.href = '/user';
-        } else {
-          alert("Register 失败")
-          window.location.href = '/user';
-        }
+        } 
       })
-      .then((result) => console.log(result))
+      .then((result) => {
+        alert("Register 失败")
+        console.log(result)
+      })
       .catch((error) => alert(error));
 
   }
