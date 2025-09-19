8b4e161bd40262ff6ed8891ba8c6015c20e04c8b
tokiya_desktop <e2544733@outlook.com>
2025-09-19 09:28:02 +0800

    mark: 转AI重构

---
+++ src/app/components/circle/RoatatingCircle.tsx
@@ -0,0 +1,18 @@
+'use client'
+import {motion} from "framer-motion";
+
+
+
+export default  function RoatatingCircle() {
+
+
+    return (
+        <>
+
+            <motion.img
+                src={"img/circle/circle_white.png"}
+                className={`mx-auto`}
+            />
+        </>
+    )
+}
