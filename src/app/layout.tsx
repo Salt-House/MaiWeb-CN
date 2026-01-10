25bd73184042932a2e053ff341a30f51ed1fc55d
tokiya <e2544733@outlook.com>
2026-01-10 12:58:53 +0800

    fix: 移除Content-Security-Policy元标签

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <link
           rel="stylesheet"
           href="https://chinese-fonts-cdn.deno.dev/packages/dymh/dist/DouyinSansBold/result.css"
