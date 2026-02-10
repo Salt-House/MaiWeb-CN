ed32bea72926ee7dcdb8ad7defaa1814f43dbc6f
TokiyaSuzuki <131091356+TokiyaSzk@users.noreply.github.com>
2026-02-10 18:59:39 +0800

    Update page.tsx

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -14,7 +14,7 @@ import { AnimatePresence, motion } from "framer-motion"
 // import PageTransitionWrapper from "../components/PageTransitionWrapper"
 import { getSongs as fetchSongs, getSongRecentUpdated as fetchRecentUpdated } from "@/services/music"
 
-const currentVersion = "25006"
+const currentVersion = "25008"
 
 const version: (keyof typeof versionIds)[] = [
   "maimai",
